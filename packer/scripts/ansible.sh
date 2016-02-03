b4c20cb8aaa968e096adef56b3ef85bd96741a84
Marco Ferrari <ferrari.marco@gmail.com>
2016-02-03 14:49:43 +0100

    Install Ansible dependencies when provisioning with Packer

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -5,3 +5,5 @@ cd /home/user/workspace
 git clone git://github.com/ansible/ansible.git --recursive
 cd ./ansible
 source ./hacking/env-setup
+apt-get install python-pip
+pip install paramiko PyYAML Jinja2 httplib2 six
