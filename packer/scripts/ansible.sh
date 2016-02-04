99b0fa08d5818e303350791c4cf3ccb0c2cf8b58
Marco Ferrari <ferrari.marco@gmail.com>
2016-02-04 09:59:52 +0100

    Reordered provisioning shell script actions

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Install required packages
+apt-get -y install git python-pip python-dev build-essential
+pip install paramiko PyYAML Jinja2 httplib2 six
+
 # Install Ansible from source
 mkdir /home/user/workspace
 cd /home/user/workspace
-apt-get -y install git python-pip python-dev build-essential
 git clone git://github.com/ansible/ansible.git --recursive
 cd ./ansible
 source ./hacking/env-setup
-pip install paramiko PyYAML Jinja2 httplib2 six
