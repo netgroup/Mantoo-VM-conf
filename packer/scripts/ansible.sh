0d7ef7e4c613f899e795ecd3905fed0c49459bdb
Marco Ferrari <ferrari.marco@gmail.com>
2016-02-04 09:31:46 +0100

    Force confirmation when installing apt-get packages

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Install Ansible from source
+mkdir /home/user/workspace
 cd /home/user/workspace
+apt-get -y install git python-pip python-dev build-essential
 git clone git://github.com/ansible/ansible.git --recursive
 cd ./ansible
 source ./hacking/env-setup
-apt-get install python-pip
 pip install paramiko PyYAML Jinja2 httplib2 six
