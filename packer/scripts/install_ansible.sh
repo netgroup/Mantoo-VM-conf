49f0a50c96f3dce917b4c5ff2c0c0c9de1cbee3f
Marco Ferrari <ferrari.marco@gmail.com>
2016-02-05 15:52:44 +0100

    Switched back to managed Ansible installation

--- packer/scripts/install_ansible.sh
+++ packer/scripts/install_ansible.sh
@@ -4,15 +4,8 @@
 set -x
 
 # Install required packages
-apt-get -y install git python-pip python-dev build-essential
-pip install paramiko PyYAML Jinja2 httplib2 six
-
-# Install Ansible from source
-cd /opt
-git clone git://github.com/ansible/ansible.git --recursive
-cd ./ansible
-source ./hacking/env-setup
-
-# Put Ansible in PATH
-echo "export PATH=/opt/ansible/bin:$PATH" >> /etc/profile.d/ansible.sh
-chmod +x /etc/profile.d/ansible.sh
+apt-get update
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
